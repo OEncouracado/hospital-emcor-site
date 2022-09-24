57968978d29e4b7fb1face1702e28a2409b550c5
OEncouraçado <tryironcl4d@gmail.com>
2022-09-23 21:51:55 -0300

    criação do component Iframe

---
+++ src/components/Iframe.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+function Iframe() {
+    return ( 
+        <iframe 
+  id="frame" 
+  className='w-100' 
+  style={{height:'600px'}} 
+  title='medcloud' 
+  src="https://www.medcloud.co/?page=emcor"
+  frameborder="0"/>
+     );
+}
+
+export default Iframe;
